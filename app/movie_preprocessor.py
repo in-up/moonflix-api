e0441b8784184a0fff80eae4f96845468d8e0922
in-up <inup@kyonggi.ac.kr>
2024-04-30 22:57:49 +0900

    feat: change dataset preprocessor

--- app/movie_preprocessor.py
+++ app/movie_preprocessor.py
@@ -22,15 +22,35 @@ def add_rating(df):
     return rating_added_df
 
 
+def add_poster(df):
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        tmdb_id = row["tmdbId"]
+        tmdb_url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?api_key={API_KEY}&language=en-US"
+        result = requests.get(tmdb_url)
+        # final url : https://image.tmdb.org/t/p/original/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg
+        try:
+            df.at[i, "poster_path"] = "https://image.tmdb.org/t/p/original" + result.json()['poster_path']
+        except (TypeError, KeyError) as e:
+            # toy story poster as default
+            df.at[i, "poster_path"] = "https://image.tmdb.org/t/p/original/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg"
+    return df
+
+
 if __name__ == "__main__":
+    # movies_df = pd.read_csv('data/movies.csv')
+    # print(movies_df)
+    
     load_dotenv()
     API_KEY = os.getenv("API_KEY")
-
+    # print(API_KEY)
+    
     movies_df = pd.read_csv('data/movies.csv')
     movies_df['movieId'] = movies_df['movieId'].astype(str)
     links_df = pd.read_csv('data/links.csv', dtype=str)
     merged_df = movies_df.merge(links_df, on='movieId', how='left')
     merged_df['url'] = merged_df['imdbId'].apply(lambda x: add_url(x))
     result_df = add_rating(merged_df)
+    result_df['poster_path'] = None
+    result_df = add_poster(result_df)
 
-    result_df.to_csv("data/movies_sort.csv", index=None)
+    result_df.to_csv("data/movies_final.csv", index=None)
